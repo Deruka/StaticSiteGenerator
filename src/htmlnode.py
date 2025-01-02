59a225a3f9fc565b4704f248054ec72bec4b1edc
Deruka <deruka.km@googlemail.com>
2025-01-02 21:21:05 +0100

    added child class LeafNode with separate to_html method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -17,3 +17,15 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Leaf Node has no Value")
+        if not self.tag:
+            return str(self.value)
+        props_string = self.props_to_html()
+        return f"<{self.tag}{props_string}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_no_props(self):
@@ -14,6 +14,23 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_multiple_props(self):
         node = HTMLNode("h1", "This is a test with three props", None, {"map1": "Ancient Forest", "map2": "Wildspire Waste", "map3": "Coral Highlands"})
         self.assertEqual(node.props_to_html(), ' map1="Ancient Forest" map2="Wildspire Waste" map3="Coral Highlands"')
+
+    def test_LeafNode_to_html_no_props(self):
+        lnode = LeafNode("p", "This is a normal text.")
+        self.assertEqual(lnode.to_html(), "<p>This is a normal text.</p>")
+
+    def test_LeafNode_to_html_no_tag(self):
+        lnode = LeafNode("", "Here is no tag to use.")
+        self.assertEqual(lnode.to_html(), "Here is no tag to use.")
+
+    def test_LeafNode_to_html_no_value(self):
+        with self.assertRaises(ValueError):
+            lnode = LeafNode("h1", "")
+            lnode.to_html()
+
+    def test_LeafNode_to_html_props(self):
+        lnode = LeafNode("a", "Click here!", {"href": "https://www.5ushiroll.com"})
+        self.assertEqual(lnode.to_html(), '<a href="https://www.5ushiroll.com">Click here!</a>')
 
 if __name__ == "__main__":
     unittest.main()
