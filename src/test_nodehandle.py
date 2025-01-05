c9fed1303262be1be40ada04acdbebcfdfcc1e06
Deruka <deruka.km@googlemail.com>
2025-01-05 16:12:21 +0100

    added methods to extract markdown images and links in nodehandle with tests

--- src/test_nodehandle.py
+++ src/test_nodehandle.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from nodehandle import split_nodes_delimiter
+from nodehandle import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 class TestNodehandle(unittest.TestCase):
     def test_singlenode_one_delimiter(self):
@@ -52,6 +52,37 @@ class TestNodehandle(unittest.TestCase):
         self.assertEqual(new_nodes, [TextNode("This delimiter ", TextType.TEXT),
                                      TextNode("", TextType.CODE),
                                      TextNode(" is empty", TextType.TEXT),])              
+
+    def test_extract_markdown_2_img(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')])
+
+    def test_extract_markdown_3_img(self):
+        text = "This is text with a ![crunchy cat luna](https://pbs.twimg.com/media/GFpyGffWIAAawRM.jpg), a ![husky](https://www.rinti.de/fileadmin/_processed_/8/2/csm_Husky-Haltung_c825940057.jpg) and ![luke skywalker](https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_7ffe21c7.jpeg?region=130%2C147%2C1417%2C796)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [('crunchy cat luna', 'https://pbs.twimg.com/media/GFpyGffWIAAawRM.jpg'), ('husky', 'https://www.rinti.de/fileadmin/_processed_/8/2/csm_Husky-Haltung_c825940057.jpg'), ('luke skywalker', 'https://lumiere-a.akamaihd.net/v1/images/luke-skywalker-main_7ffe21c7.jpeg?region=130%2C147%2C1417%2C796')])
+
+    def test_extract_markdown_no_img(self):
+        text = "This is text with no images at all"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [])
+
+    def test_extract_markdown_2_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [('to boot dev', 'https://www.boot.dev'), ('to youtube', 'https://www.youtube.com/@bootdotdev')])
+
+    def test_extract_markdown_1_link(self):
+        text = "This is text with a link [to the artist 5ushiroll](https://www.5ushiroll.com)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [('to the artist 5ushiroll', 'https://www.5ushiroll.com')])
+
+    def test_extract_markdown_no_links(self):
+        text = "This is text with no links"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [])
+
 
 if __name__ == "__main__":
     unittest.main()
