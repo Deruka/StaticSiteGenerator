e8ff292eed9bf33e3e5c3c5ec93ef0b8a99116e1
Deruka <deruka.km@googlemail.com>
2025-01-12 20:28:52 +0100

    changes on main.py to create public from static, added public to gitignore

--- src/main.py
+++ src/main.py
@@ -1,7 +1,31 @@
-from textnode import TextNode, TextType
+import os, shutil
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    source = 'static'
+    destination = 'public'
+    copy_directory(source, destination)
+
+
+def copy_directory(source, destination):
+    # Delete destination if it exists
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+
+    # Make sure destination exists
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    
+    # Get list of everything in source directory
+    for item in os.listdir(source):
+        # Create full paths
+        source_path = os.path.join(source, item)
+        dest_path = os.path.join(destination, item)
+
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, dest_path)
+            print(f"file {item} copied from {source} to {destination}")
+        else:
+            print(f"opening directory {source_path}")
+            copy_directory(source_path, dest_path)       
 
 main()
