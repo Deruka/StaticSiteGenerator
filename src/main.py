27294a0dfc8081a13306f42b23889d2adbb8339d
Deruka <deruka.km@googlemail.com>
2025-01-13 16:20:42 +0100

    added documentary inside code what happens at the steps

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ import os, shutil
 from nodehandle import markdown_to_html_node, extract_title
 
 def main():
+    # static variables for the source and destination folders
     source = 'static'
     destination = 'public'
+    # copy static to public
     copy_directory(source, destination)
     # Generate the page
     generate_pages_recursive(
@@ -27,6 +29,7 @@ def copy_directory(source, destination):
         source_path = os.path.join(source, item)
         dest_path = os.path.join(destination, item)
 
+        # if source_path is a file, copy it to destination, otherwise open the folder and recursive call the function again.
         if os.path.isfile(source_path):
             shutil.copy(source_path, dest_path)
             print(f"file {item} copied from {source} to {destination}")
@@ -35,31 +38,42 @@ def copy_directory(source, destination):
             copy_directory(source_path, dest_path)       
 
 def generate_page(from_path, template_path, dest_path):
+    # Message for terminal
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    # Read Markdown and template
     with open(from_path, 'r') as file:
         markdown = file.read()
     with open(template_path, 'r') as file:
         template = file.read()
+
+    # Put markdown into HTML Nodes and create HTML-String
     get_html = markdown_to_html_node(markdown)
     new_html = get_html.to_html()
+    # Extract title for template
     html_title = extract_title(markdown)
+    # replace title and content in the template
     template = template.replace("{{ Title }}",html_title)
     template = template.replace("{{ Content }}", new_html)
+    # Create directories if needed and do not exist
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    # Write template in destination with created Markdown
     with open(dest_path, 'w') as file:
         file.write(template)
 
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    # Message for terminal
     print(f"Generating page from {dir_path_content} to {dest_dir_path} using {template_path}")
 
     # Make sure destination exists
     if not os.path.exists(dest_dir_path):
         os.mkdir(dest_dir_path)
 
+    # Check all items in the content folder, that needs to be created
     for item in os.listdir(dir_path_content):
         source_path = os.path.join(dir_path_content, item)
         dest_path = os.path.join(dest_dir_path, item)
 
+        # In case the file ends with ".md", turn it into ".html"
         if os.path.isfile(source_path) and source_path.endswith(".md"):
             dest_path = dest_path.replace(".md", ".html")
             
--- src/nodehandle.py
+++ src/nodehandle.py
@@ -2,106 +2,164 @@ from textnode import TextNode, TextType, text_node_to_html_node
 from htmlnode import ParentNode, LeafNode
 import re
 
+# splitting up nodes if one node has one large text with other formatting like **bold** or *italic*
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
+    # iterate through all nodes
     for node in old_nodes:
+        # if node is already TextType.TEXT, add it to the new nodes and go to the next node
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
+        # if node is not a TextType.TEXT, the node has to be split up and the parts of the specific TextType will get converted
         else:
             wip_node = node.text.split(delimiter)
+            # if remainder of the node is 0, it means we are missing the delimiter, we need an odd number of entries in the to working nodes.
             if len(wip_node) % 2 == 0:
                 raise Exception(f"Missing closing delimiter: {delimiter}")
+            # using a boolean inside the delimiter to swap between the intended text_type and TextType.TEXT
             inside_delimiter = False
+            # going through all the parts of the node, starting with the TextType.TEXT, then delimiter, rinse and repeat.
             for part in wip_node:
                 if inside_delimiter:
                     new_nodes.append(TextNode(part, text_type))
                 else:
                     new_nodes.append(TextNode(part,TextType.TEXT))
+                # swap boolean
                 inside_delimiter = not inside_delimiter        
     return new_nodes
 
 def extract_markdown_images(text):
+    # regex for finding image links in a markdown: ![alt text](URL)
     matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
 
 def extract_markdown_links(text):
+    # regex for finding url links in a markdown: [url text](URL)
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
 
 def split_nodes_image(old_nodes):
+    # similar to split_nodes_delimiter, just focused on images only
     new_nodes = []
+    # iterate through all nodes
     for node in old_nodes:
+        # get a tuple with the image text and link
         node_img = extract_markdown_images(node.text)
+        # if there is no link, just append the current node and go to the next one
         if len(node_img) == 0:
             new_nodes.append(node)
-        else:
+        else: 
+            # Put all the text of the current node in a variable to check for matches
             remaining_text = node.text
             while remaining_text:
+                # keep working on matches in case there is more than one image in the markdown
                 matches = extract_markdown_images(remaining_text)
+                # if no match can be found, break the loop
                 if not matches:
                     break
+                # workable variables for the found matches to filter the text before and after
                 image_alt, image_link = matches[0]
+                # split up the remaining text to before and after the image
                 parts = remaining_text.split(f"![{image_alt}]({image_link})", 1)
+                # if the part before the image is not empty, create a TextNode with all the text in it.
                 if parts[0] != "":
                     new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                # create the image TextNode
                 new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+                # reduce the remaining_text with the already worked through text and check for a next possible match
                 remaining_text = parts[1]
+            # if remaining text is not empty and no match was found, create the final TextNode
             if remaining_text != "":
                 new_nodes.append(TextNode(remaining_text, TextType.TEXT))
     return new_nodes
 
 def split_nodes_link(old_nodes):
+    # similar to split_nodes_delimiter, just focused on images only
     new_nodes = []
+    # iterate through all nodes
     for node in old_nodes:
+        # get a tuple with the url text and link
         node_lnk = extract_markdown_links(node.text)
+        # if there is no link, just append the current node and go to the next one
         if len(node_lnk) == 0:
             new_nodes.append(node)
         else:
+             # Put all the text of the current node in a variable to check for matches
             remaining_text = node.text
             while remaining_text:
+                # keep working on matches in case there is more than one link in the markdown
                 matches = extract_markdown_links(remaining_text)
+                # if no match can be found, break the loop
                 if not matches:
                     break
+                # workable variables for the found matches to filter the text before and after
                 link_txt, link_url = matches[0]
+                # split up the remaining text to before and after the link
                 parts = remaining_text.split(f"[{link_txt}]({link_url})", 1)
+                 # if the part before the link is not empty, create a TextNode with all the text in it.
                 if parts[0] != "":
                     new_nodes.append(TextNode(parts[0], TextType.TEXT))
+                # create the link TextNode
                 new_nodes.append(TextNode(link_txt, TextType.LINK, link_url))
+                # reduce the remaining_text with the already worked through text and check for a next possible match
                 remaining_text = parts[1]
+            # if remaining text is not empty and no match was found, create the final TextNode
             if remaining_text != "":
                 new_nodes.append(TextNode(remaining_text, TextType.TEXT))
     return new_nodes
 
 def text_to_textnodes(text):
+    # create a textnode with all the input text (further down I could've kept using the same variable and just 
+    # alter the variable every time, but I wanted different names for clear readabilty)
     nodes = [TextNode(text, TextType.TEXT)]
+    # check the new node for images
     nodes_after_images = split_nodes_image(nodes)
+    # check the new node for links
     nodes_after_links = split_nodes_link(nodes_after_images)
+    # check the new node for code blocks
     nodes_after_code = split_nodes_delimiter(nodes_after_links, "`", TextType.CODE)
+    # check the new node for bold text
     nodes_after_bold = split_nodes_delimiter(nodes_after_code, "**", TextType.BOLD)
+    # check the new node for italic text
     final_nodes = split_nodes_delimiter(nodes_after_bold, "*", TextType.ITALIC)
+    # return the final node list with all nodes from the original input markdown
     return final_nodes
 
 def markdown_to_blocks(markdown):
     md_blocks = []
+    # remove all whitespaces in the markdown and make a clear split for new line parameters 
+    # with this regex to create all markdown parts into blocks
     wip_md = re.sub(r'\n\s*\n', '\n\n', markdown)
+    # split the blocks to iterate through them after
     wip_blocks = wip_md.split("\n\n")
     for block in wip_blocks:
+        # split each block in lines
         lines = block.split("\n")
+        # strip the block of any whitespaces
         cleaned_lines = [line.strip() for line in lines]
+        # rejoin all the lines 
         cleaned_block = '\n'.join(cleaned_lines)
+        # if a cleaned block exist, add it to the final list and return it after iterating through it all
         if cleaned_block:
             md_blocks.append(cleaned_block)
     return md_blocks
 
 def block_to_block_type(markdown):
+    # this function checks all block types and returns the type of markdown block
+    # regex looks for "#" at the beginning of the line, from 1 to 6 times for headings
     if re.match(r'^#{1,6} ', markdown):
         return "heading"
+    # regex looks for the code block "```" at the start and end of a string
     elif re.match(r'^```[\s\S]*```$', markdown, re.DOTALL):
         return "code"
+    # regex looks for lines that start with "> " in all lines for quotes
     elif all(line.startswith('> ') for line in markdown.splitlines()):
         return "quote"
+    # regex looks for either "*" or "-" in all lines to match for an unordered list
     elif all(re.match(r'^[*-] ', line) for line in markdown.splitlines()):
         return "unordered_list"
+    # regex looks for numbered listing in all lines to match for an ordered list
+    # also checks if the numbers itself are ordered. In case it does not match, it returns as a paragraph
     elif all(re.match(r'^\d+\. ', line) for line in markdown.splitlines()):
         lines = markdown.splitlines()
         numbers = [int(re.match(r'^(\d+)\. ', line).group(1)) for line in lines]
@@ -109,6 +167,7 @@ def block_to_block_type(markdown):
             return "ordered_list"
         else:
             return "paragraph"
+    # if neither of the options fit, it returns as a paragraph
     else:
         return "paragraph"
     
